30806744b09adaabd703c26c7b87dfca777992b6
Zhanna Suianina <cezyby3@gmail.com>
2024-04-25 01:26:56 +0300

    adjust

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@
 
 function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
-    const makeTransaction = `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
+    const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     return makeTransaction; 
 }
 
--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@
 
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
-    const getShippingMessage = `"Shipping to ${country} will cost ${totalPrice} credits"`;
+    const getShippingMessage = `Shipping to ${country} will cost ${totalPrice} credits`;
     return getShippingMessage; 
 }
 
